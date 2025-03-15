58fdec160ae8b073fd02e7f7d579f424db0b6760
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2025-03-15 15:07:09 +0700

    move url-shortcut
    Signed-off-by: Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-# repos="design-patterns url-shortcut django-app-deployment stupid-app Algorithm simple-calculator"
+# repos="url-shortcut django-app-deployment stupid-app Algorithm simple-calculator"
 
 repo=$1
 git remote add --fetch $repo git@gitlab.com:Magiskb0Y/$repo
 git merge --allow-unrelated-histories $repo/master
+mkdir $repo
